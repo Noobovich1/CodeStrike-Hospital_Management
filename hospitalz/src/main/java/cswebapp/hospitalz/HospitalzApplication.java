744a4dde9359c7b7af77d50149f5de2e09359a47
Noobovich1 <siper.chicken@gmail.com>
2026-05-12 23:12:28 +0700

    first commit

---
+++ hospitalz/src/main/java/cswebapp/hospitalz/HospitalzApplication.java
@@ -0,0 +1,13 @@
+package cswebapp.hospitalz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalzApplication.class, args);
+	}
+
+}
---
+++ hospitalz/src/test/java/cswebapp/hospitalz/HospitalzApplicationTests.java
@@ -0,0 +1,13 @@
+package cswebapp.hospitalz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
